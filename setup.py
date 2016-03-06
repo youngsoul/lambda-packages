1bae2134d579f3d4f99e1d3c81f926c9efb61284
Rich Jones <rich@anomos.info>
2016-03-06 18:57:15 +0100

    begin packagification

---
+++ setup.py
@@ -0,0 +1,40 @@
+import os
+import setuptools
+import sys
+
+from setuptools import setup
+
+# Set external files
+try:
+    from pypandoc import convert
+    README = convert('README.md', 'rst')	 
+except ImportError:
+    README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='lambda_packages',
+    version='0.0.1',
+    packages=['lambda_packages'],
+    include_package_data=True,
+    license='MIT License',
+    description='AWS Lambda Packages',
+    long_description=README,
+    url='https://github.com/Miserlou/lambda-packages',
+    author='Rich Jones',
+    author_email='rich@openwatch.net',
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
