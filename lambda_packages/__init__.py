910052a0884f48b00146c0805a1f05c18243f1e0
Jonny Morrill <j.morrill@samsung.com>
2016-07-21 16:44:25 -0700

    API requires that the manifest name matches the pypi package name

--- lambda_packages/__init__.py
+++ lambda_packages/__init__.py
@@ -26,7 +26,7 @@ lambda_packages = {
         'version': '2.6.1',
         'path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'psycopg2', 'psycopg2-2.6.1.tar.gz')
     },
-    'Crypto': {
+    'pycrypto': {
         'version': '2.6.1',
         'path': os.path.join(os.path.dirname(os.path.abspath(__file__)),
                              'pycrypto', 'pycrypto-2.6.1.tar.gz')
