f9f20860b9c788a467fb3dfbe04413cec857ff24
Luke Phillippi <luke@lukephillippi.com>
2016-12-11 10:14:24 -0500

    Added misaka 2.0.0 package.

---
+++ lambda_packages/misaka/build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+PACKAGE=${1}
+VERSION=${2}
+TMP_DIR="${PACKAGE}_${VERSION}"
+
+mkdir ${TMP_DIR}
+cd  ${TMP_DIR}
+echo "Packaging ${PACKAGE}"
+
+echo "do update"
+sudo yum update -y
+
+sudo yum groupinstall -y "Development Tools"
+
+echo "do dependcy install"
+
+sudo yum install -y libffi libffi-devel openssl openssl-devel
+
+ENV="env-${PACKAGE}-${VERSION}"
+
+echo "make ${ENV}"
+virtualenv "${ENV}"
+
+echo "activate env in `pwd`"
+source "${ENV}/bin/activate"
+
+# https://github.com/pypa/pip/issues/3056
+echo '[install]' > ./setup.cfg
+echo 'install-purelib=$base/lib64/python' >> ./setup.cfg
+
+
+TARGET_DIR=${ENV}/packaged
+echo "install pips"
+pip install --verbose --use-wheel --no-dependencies --target ${TARGET_DIR} "${PACKAGE}==${VERSION}"
+deactivate
+
+cd ${TARGET_DIR} && tar -zcvf ../../../${PACKAGE}-${VERSION}.tar.gz * && cd ../../..
+rm -rf ${TMP_DIR}
+
+
+
+
