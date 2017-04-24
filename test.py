e7f4f2300d5b7476fc068e3c4940258bba007d44
nik <nb@velorum.net>
2017-04-24 21:48:54 +0100

    Updating manifest to a better schema version.

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ from lambda_packages import lambda_packages
 from nose.tools import assert_true
 
 def test_packages():
-    for name, details in lambda_packages.items():
-        assert_true(os.path.exists(details['path']))
-        assert_true(os.path.exists(details['python2.7-path']))
+    for name, package in lambda_packages.items():
+        for runtime, details in package.items():
+            package_file_name = os.path.split(details['path'])[1]
 
-        if 'python3.6-path' in details:
-            assert_true(os.path.exists(details['python3.6-path']))
+            assert_true(package_file_name.startswith(runtime + "-"))
+            assert_true(os.path.exists(details['path']))
