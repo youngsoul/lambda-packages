425dde51651f68e5813e59cf0b5ca9536101d1dc
Rich Jones <rich@anomos.info>
2017-06-05 17:56:44 -0400

    0.15.1 - bump cryptography version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='lambda_packages',
-    version='0.15.0',
+    version='0.15.1',
     packages=['lambda_packages'],
     include_package_data=True,
     license='MIT License',
