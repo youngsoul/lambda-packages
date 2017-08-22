3832d2da601a77494fe069ebc11fc4314ef5f38e
Rich Jones <rich@anomos.info>
2017-08-22 16:31:24 -0400

    clean up manifest format

--- lambda_packages/__init__.py
+++ lambda_packages/__init__.py
@@ -34,9 +34,9 @@ lambda_packages = {
             'path': os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                  'OpenCV', 'python2.7-OpenCV-3.1.0.tar.gz')
         },
-		'python3.6': {
-			'version': '3.3.0',
-			'path': os.path.join(os.path.dirname(os.path.abspath(__file__)),
+        'python3.6': {
+	    'version': '3.3.0',
+            'path': os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                  'OpenCV', 'python3.6-OpenCV-3.3.0.tar.gz')
 		}
     },
